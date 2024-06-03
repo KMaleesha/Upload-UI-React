c25ed5619c858f0a1805622f1715c67dc7ea04dd
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-06-03 14:54:15 +0530

    file upload ui

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import FileUpload from './Components/File Upload/Fileupload';
+
+function App() {
+  return (
+    <div className='container'>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
